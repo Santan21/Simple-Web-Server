8b88831cac1f952297b7f5d4f07317b5ad6732b8
Santan21 <lesliegyamfi02@gmail.com>
2023-09-09 20:23:06 +0000

    first commit here

---
+++ index.js
@@ -0,0 +1,12 @@
+const require = require ('http')
+
+const server = http.createServer(
+    (req, res) => {
+        res.end('A Simple Web Server')
+    }
+);
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening live on port 8000');
+});
+    
