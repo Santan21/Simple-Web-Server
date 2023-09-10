ae0f6c5d7d33e8742420b4acf0b6b3be6da8773a
Santan21 <lesliegyamfi02@gmail.com>
2023-09-10 15:01:16 +0000

    added pathName

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const http = require ('http');
+const url = require ('url');
 
-const server = http.createServer(
-    
-    (req, res) => {
-        console.log(req);
-        res.end('A Simple Web Server')
-        
-    }
-);
+
+const server = http.createServer((req, res) => {
+         const pathName = req.url;
+         res.end('A Simple Web Server')       
+    });
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening live on port 8000');
