50b1cb92f98bb8679a2ed2e075a4f25802c5f804
Santan21 <lesliegyamfi02@gmail.com>
2023-09-22 13:41:22 +0000

    Experimenting Routing on a Simple Web Server

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const url = require ('url');
 
 
 const server = http.createServer((req, res) => {
-         const pathName = req.url;
-         res.end('A Simple Web Server')       
+const pathName = req.url;
+if ( pathName === '/' || pathName === '/overview'){
+res.end('This is the overview');
+}
+    else if (pathName === '/product') {
+        res.end('This is the product');
+    }
+    else 
+        res.writeHead(404, {
+        
+    });
+    
+
+
+         res.end('A Simple Web Server')     
+
     });
 
-server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening live on port 8000');
+    server.listen(4000, '127.0.0.1', () => {
+
+    console.log('Listening live on port 4000');
 });
     
